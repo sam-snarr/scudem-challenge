b4acedffd9fda2c3c241a61e3e7287ea1ca030fe
Sam Snarr <38705645+sam-snarr@users.noreply.github.com>
2019-12-13 12:58:52 -0500

    Delete print_figs.m

--- print_figs.m
+++
@@ -1,6 +0,0 @@
-
-
-print(figure(1),'g3_fad1.png','-dpng','-r800')
-print(figure(2),'g3_fad2.png','-dpng','-r800')
-print(figure(3),'g3_fad3.png','-dpng','-r800')
-print(figure(4),'g3_fad4.png','-dpng','-r800')
